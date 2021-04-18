bdedbc27f61667c32664258148831d9add61a096
piyush101 <piyush9797gupta@gmail.com>
2021-04-19 02:30:47 +0530

    Updated code for foreground and background notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_news/route_generator.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -11,8 +12,13 @@ import 'components/provider/dark_theme_provider.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
 
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
   return runApp(ChangeNotifierProvider(
     child: MyApp(),
     create: (BuildContext context) =>
@@ -20,15 +26,72 @@ Future<void> main() async {
   ));
 }
 
-class MyApp extends StatelessWidget {
+getToken() async {
+  String token = await FirebaseMessaging.instance.getToken();
+  print(token);
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+  print('Title is '+message.notification.title);
+  print('Body is '+message.notification.body);
+
+}
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  'This channel is used for important notifications.', // description
+  importance: Importance.high,
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channel.description,
+                // TODO add a proper drawable resource to android, for now using
+                //      one that already exists in example app.
+                icon: 'launch_background',
+              ),
+            ));
+      }
+    });
+    getToken();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<DarkThemeProvider>(
         builder: (context, DarkThemeProvider darkThemeProvider, child) {
       return MultiProvider(
-        providers: [StreamProvider.value(value: FirebaseAuth.instance.authStateChanges())],
+        providers: [
+          StreamProvider.value(value: FirebaseAuth.instance.authStateChanges())
+        ],
         child: MaterialApp(
-          theme: darkThemeProvider.getTheme,  
+          theme: darkThemeProvider.getTheme,
           debugShowCheckedModeBanner: false,
           initialRoute: '/',
           onGenerateRoute: RouteGenerator.generateRoute,
